53ee1f70ce9bcdc3f5b4b96cab6e9dd84a287a1c
wwalpha <wwalpha@gmail.com>
2022-12-07 01:02:47 +0000

    update

--- terraform/datafactory/integration_runtime.tf
+++ terraform/datafactory/integration_runtime.tf
@@ -3,8 +3,8 @@ resource "azurerm_data_factory_integration_runtime_azure" "this" {
   data_factory_id         = azurerm_data_factory.this.id
   location                = var.resource_group_location
   virtual_network_enabled = true
-  # cleanup_enabled         = true
-  # time_to_live_min        = 60
+  cleanup_enabled         = true
+  time_to_live_min        = 60
 }
 
 resource "azurerm_data_factory_integration_runtime_self_hosted" "this" {
